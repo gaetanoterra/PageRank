2bd47dca53ded35981c9b678643cbb82da1ca6f0
gaetanoterra <73600008+gaetanoterra@users.noreply.github.com>
2021-09-22 15:26:54 +0200

    Update PageRankSpark.py

--- page_rank_algorithm_spark/PageRankSpark.py
+++ page_rank_algorithm_spark/PageRankSpark.py
@@ -23,7 +23,8 @@ def getLinkContribution(line):
     if len(line[1]) > 0:
         for link in line[1]:
             yield link, line[2]
-
+    else:
+        yield line[0], 0
 #function that computes page_rank, returns => node, page_rank
 def rankingcalculus(line):
     return line[0], (alpha_value/node_number + (1-alpha_value) * line[1])
@@ -37,42 +38,36 @@ def rewrite(line):
 
 if __name__ == "__main__":
     input_file_name = sys.argv[1]
-    alpha_value = float(sys.argv[2])
-    iterations_number = int(sys.argv[3])
-
+    output_file_name = sys.argv[2]
+    alpha_value = float(sys.argv[3])
+    iterations_number = int(sys.argv[4])
     sc = SparkContext("yarn", "PageRank")
-
     text = sc.textFile(input_file_name)
-
     #counting the nodes (pages) of the input document
     node_number = text.count()
-
     #call the findTitleAndLinks function on the document to select titles and links
-    title_links_rdd = text.map(lambda line : findTitleAndLinks(line))
-
+    title_links_rdd = text.map(lambda line : findTitleAndLinks(line)).cache()
     initial_page_rank = 1/node_number
-
-  # I reorder the data by putting node_name, links, initial_page_rank
-    node_rank_rdd = title_links_rdd.map(lambda line : (line[0], line[1], initial_page_rank))
-
+    # I reorder the data by putting node_name, initial_page_rank
+    node_rank_rdd = title_links_rdd.map(lambda line : (line[0], initial_page_rank))
     for i in range(iterations_number):
+        # i make a join between (node, links) and (node, page rank) obtaining -> node, (links, pagerank) -> after rewrite -> node, links, pagerank
+        node_rank_rdd = title_links_rdd.join(node_rank_rdd).map(lambda line : rewrite(line))
         # Call the contributeCalculus function to calculate the contribution each node would make to its links
         contribute_ranking_rdd = node_rank_rdd.map(lambda line : contributeCalculus(line))
-
-        # Call the getLinkContribution function to associate the contribution it receives with each link
+        # Call the getLinkContribution function to associate the contribution it receives with each link (node that have no inlinks are excluded brcause they are not links)
         link_contribution_association_rdd = contribute_ranking_rdd.flatMap(lambda line : getLinkContribution(line))
-
-        #Group the nodes that have the same key (the same name) and add the partial page_ranks; then I calculate the ranking with rankingcalculus
+        # Group the nodes that have the same key (the same name) and add the partial page_ranks; then I calculate the ranking with rankingcalculus
         somma_rdd = link_contribution_association_rdd.reduceByKey(lambda x, y : x + y).map(lambda line : rankingcalculus(line))
-
-        # Join the newly calculated ranks with the nodes nobody points to (they don't receive any contribution of page_rank from other nodes so their page_rank remains constant)
-        node_rank_rdd = title_links_rdd.leftOuterJoin(somma_rdd).map(lambda line : rewrite(line))
-
+        # We associate the value alpha/N to the nodes with no inlinks 
+        no_inlink_nodes_rdd = title_links_rdd.map(lambda node: (node[0], alpha_value*(1/node_number))).subtractByKey(somma_rdd)
+        # We put all nodes (nodes with no inlinks and with inlinks) together
+        node_rank_rdd = somma_rdd.union(no_inlink_nodes_rdd)
     # Sort in descending order according to rank and output node + page_rank
-    sort_rdd = node_rank_rdd.sortBy(lambda x: -x[2]).map(lambda line : (line[0], line[2]))
+    sort_rdd = node_rank_rdd.sortBy(lambda x: -x[1]).map(lambda line : (line[0], line[1]))
     for element in sort_rdd.collect():
         print("somma_rdd: " + str(element))
-
     #save the output to a file
-    sort_rdd.saveAsTextFile("spark-output")
+    sort_rdd.saveAsTextFile(output_file_name)
+
 
